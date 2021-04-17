b2ea5181e8f21a9c818e8bae13a64b02bd8233d5
peterbeleganski <pepibeleganski@gmail.com>
2021-04-18 02:25:25 +0300

    init

---
+++ src/main/java/com/restaurant/management/RmsApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsApplication.class, args);
+	}
+
+}
