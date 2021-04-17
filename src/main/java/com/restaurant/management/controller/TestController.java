199c8c10bff9c7b7c7c9b29abe7ee1b64a2c7ce2
peterbeleganski <pepibeleganski@gmail.com>
2021-04-18 02:29:42 +0300

    temp

--- src/main/java/com/restaurant/management/controller/TestController.java
+++ src/main/java/com/restaurant/management/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+ //
     @GetMapping
     public String hello() {
         return "hello";
