7f47bb2bf1edfcb597db14e725b56f8aac9f1e98
peterbeleganski <pepibeleganski@gmail.com>
2021-04-18 03:15:41 +0300

    remove unused constants

--- src/main/java/com/restaurant/management/config/util/BaseConstants.java
+++ src/main/java/com/restaurant/management/config/util/BaseConstants.java
@@ -2,9 +2,6 @@ package com.restaurant.management.config.util;
 
 public final class BaseConstants {
 
-    public static final String ROLE_WAITER = "ROLE_WAITER";
-    public static final String ROLE_ADMIN = "ROLE_ADMIN";
-    public static final String ROLE_OWNER = "ROLE_OWNER";
     public static final String AUTHORIZATION = "Authorization";
 
     private BaseConstants() {
